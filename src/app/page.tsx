991e8d4e0b12f2355f21376ff3353b63ece964b4
marcossnikel <marcosnikeldev@gmail.com>
2024-02-15 19:56:07 -0300

    fix - remove no used imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { AiFillFacebook, AiFillInstagram, AiFillMail } from 'react-icons/ai'
-import { RiWhatsappFill } from 'react-icons/ri'
 
 export default function Home() {
   return (
