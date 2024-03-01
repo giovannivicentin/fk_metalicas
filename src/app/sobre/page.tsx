27568a0f59bb2be529b33cab281e04bcba196c0b
Giovanni Vicentin <giovannifvicentin@gmail.com>
2024-03-01 13:30:17 -0300

    feat: implement new routing for the about page

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,19 @@
+function AboutPage() {
+  return (
+    <div className="min-h-lvh">
+      <h1>Projetos Realizados</h1>
+      <p>
+        Explore nossos projetos recentes e <strong>clique</strong> no que chamar
+        mais a sua atenção para ver mais detalhes.
+      </p>
+      <div>
+        {/* Make a grid with all projects and for each project use a component that I 
+          will build for that, the component must have an image, title, may be the tons 
+          and local but I think doesnt need 
+          I wanna click in the project and see in full screen with a big description */}
+      </div>
+    </div>
+  )
+}
+
+export default AboutPage
