d78599fa035659fb4e2bb68090b02419b599de9a
Giovanni Vicentin <giovannifvicentin@gmail.com>
2024-03-04 19:46:46 -0300

    style: Add a link tag so that when I click on the component, it redirects to the /obras page.

--- src/components/cardItem.tsx
+++ src/components/cardItem.tsx
@@ -1,5 +1,6 @@
 import { Card, CardDescription, CardTitle } from '@/components/ui/card'
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface CardItemProps {
   src: string
@@ -10,16 +11,18 @@ interface CardItemProps {
 
 export function CardItem({ src, alt, title, description }: CardItemProps) {
   return (
-    <Card className="flex flex-col object-cover overflow-hidden border border-neutral-300">
-      <Image
-        src={src}
-        alt={alt}
-        width={160}
-        height={90}
-        className="w-full rounded-se-lg"
-      />
-      <CardTitle className="my-2 px-4">{title}</CardTitle>
-      <CardDescription className="px-4 mb-2">{description}</CardDescription>
-    </Card>
+    <Link href="obras">
+      <Card className="flex flex-col object-cover overflow-hidden border border-neutral-300">
+        <Image
+          src={src}
+          alt={alt}
+          width={160}
+          height={90}
+          className="w-full rounded-se-lg"
+        />
+        <CardTitle className="my-2 px-4">{title}</CardTitle>
+        <CardDescription className="px-4 mb-2">{description}</CardDescription>
+      </Card>
+    </Link>
   )
 }
