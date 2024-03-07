1876b603292269e75800320039c0469bb02da287
Giovanni Vicentin <78269662+giovannivicentin@users.noreply.github.com>
2024-03-07 19:47:06 -0300

    refactor: change <br/> tags to <span/> tags to be more sematic
    Co-authored-by: Marcos Nikel <101404357+marcossnikel@users.noreply.github.com>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -193,7 +193,27 @@ export function Footer() {
           <p className="text-muted text-center sm:text-end font-bold text-2xl mb-2">
             Fale conosco
           </p>
-          <p className="text-muted m-0 p-0 text-center sm:text-end">
+          <p className="text-muted m-0 p-0 text-center sm:text-end flex flex-col gap-4">
+                      <span>Horário de Funcionamento:</span>
+                      <span>Segunda a sexta</span>
+                      <span>7h às 17h</span>
+                      //.... com o resto das coisas
+     
+            <Link
+              href="https://www.facebook.com/frankmetalicas"
+              href="mailto:frank@frankmetalicas.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-muted hover:text-brand-orange"
+            >
+              <AiFillFacebook
+                className="text-neutral-200 hover:text-brand-orange transition-transform transform hover:scale-110"
+                size={30}
+              />
+              frank@frankmetalicas.com
+            </Link>
+          </p>
+          
             Horário de Funcionamento:
             <br />
             Segunda a sexta
