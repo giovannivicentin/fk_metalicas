be5a7bc573626ffed1e7b1f89ed89464baea5423
Giovanni Vicentin <78269662+giovannivicentin@users.noreply.github.com>
2024-04-09 12:43:02 +0000

    style: update padding of texts in computer screens and change the font size

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -2,18 +2,18 @@ import Image from 'next/image'
 
 function ConstructionPage() {
   return (
-    <div className="min-h-lvh flex flex-col gap-4 items-center justify-center sm:pb-32 pb-20 bg-background-color">
+    <div className="min-h-lvh flex flex-col gap-4 items-center justify-center pb-20 bg-background-color">
       <h1 className="pt-32 text-center sm:leading-relaxed text-2xl sm:text-3xl font-extrabold">
         Uma Trajetória Forjada em Aço
       </h1>
-      <p className="text-center leading-relaxed w-11/12 sm:w-3/4 text-sm sm:text-md">
+      <p className="text-center leading-relaxed w-11/12 sm:w-3/4 text-sm sm:text-lg">
         Frank Vicentin, nosso maestro das estruturas metálicas, com sua formação
         na prestigiosa Universidade Católica de Campinas, orquestra cada projeto
         com uma harmonia perfeita entre conhecimento técnico e criatividade sem
         limites. A cada desafio, a equipe da Frank Metálicas se supera,
         transformando o ordinário em extraordinário.
       </p>
-      <div className="flex flex-col-reverse sm:flex-row w-11/12 sm:w-3/4 gap-4 mt-4 sm:mt-32">
+      <div className="flex flex-col-reverse sm:flex-row w-11/12 sm:w-3/4 gap-4 mt-4 sm:mt-20">
         <Image
           src="/template.png"
           alt="template man"
@@ -25,7 +25,7 @@ function ConstructionPage() {
           <h2 className="text-center leading-relaxed text-2xl font-extrabold">
             O Aço Como Você Nunca Viu
           </h2>
-          <p className="leading-relaxed text-justify text-sm sm:text-md sm:text-justify mt-4 sm:px-8">
+          <p className="leading-relaxed text-justify text-sm sm:text-lg sm:text-justify mt-4 sm:px-8">
             Na Frank Metálicas, transcendemos a mera construção para criar
             verdadeiros legados em aço. Aqui, cada peça é esculpida com arte,
             precisão e uma visão de futuro, elevando nossas criações a ícones de
