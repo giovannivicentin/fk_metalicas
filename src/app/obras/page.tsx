e906b16d676726c489b8fb16a95a57f4de94ff1b
giovannivicentin <giovannifvicentin@gmail.com>
2024-04-10 17:51:28 -0300

    feat: create the structure of constructionPage

--- src/app/obras/page.tsx
+++ src/app/obras/page.tsx
@@ -1,17 +1,16 @@
 function ConstructionPage() {
   return (
-    <div className="min-h-lvh">
-      <h1>Projetos Realizados</h1>
-      <p>
-        Explore nossos projetos recentes e <strong>clique</strong> no que chamar
-        mais a sua atenção para ver mais detalhes.
-      </p>
-      <div>
-        {/* Make a grid with all projects and for each project use a component that I 
-          will build for that, the component must have an image, title, may be the tons 
-          and local but I think doesnt need 
-          I wanna click in the project and see in full screen with a big description */}
+    <div className="min-h-lvh items-center flex flex-col pb-20 bg-background-color">
+      <div className="animate-fade-right animate-once">
+        <h1 className="pt-32 text-start text-2xl sm:text-3xl font-extrabold">
+          Projetos Realizados
+        </h1>
+        <p className="text-start leading-relaxed text-sm sm:text-lg mt-4">
+          Explore nossos projetos recentes e <strong>clique</strong> no que
+          chamar mais a sua atenção para ver mais detalhes.
+        </p>
       </div>
+      <div className="mt-8"></div>
     </div>
   )
 }
