75f98dadd058415f9497af3871b289407ddb908b
giovannivicentin <giovannifvicentin@gmail.com>
2024-04-12 20:08:37 -0300

    feat: create the structure of constructionPage

--- src/app/obras/page.tsx
+++ src/app/obras/page.tsx
@@ -1,16 +1,30 @@
+import { ConstructionPageItem } from '@/components/constructionPageItem'
+import constructionItems from './constructionItems'
+import { ConstructionItem } from './interface'
+
 function ConstructionPage() {
   return (
-    <div className="min-h-lvh items-center flex flex-col pb-20 bg-background-color">
+    <div className="min-h-lvh mx-2 md:mx-32 pb-20 bg-background-color">
       <div className="animate-fade-right animate-once">
         <h1 className="pt-32 text-start text-2xl sm:text-3xl font-extrabold">
-          Projetos Realizados
+          Construções Realizadas
         </h1>
         <p className="text-start leading-relaxed text-sm sm:text-lg mt-4">
           Explore nossos projetos recentes e <strong>clique</strong> no que
           chamar mais a sua atenção para ver mais detalhes.
         </p>
       </div>
-      <div className="mt-8"></div>
+      <div className="mt-8 grid grid-cols-2 sm:grid-cols-4 gap-4">
+        {constructionItems.map((item: ConstructionItem) => (
+          <ConstructionPageItem
+            key={item.id}
+            src={item.src}
+            alt={item.alt}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </div>
     </div>
   )
 }
