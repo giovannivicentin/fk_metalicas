dc9086c635b53a52005145dc0b6fb23c31b4c0b5
giovannivicentin <giovannifvicentin@gmail.com>
2024-04-11 22:14:32 -0300

    feat: finish the COnstructionPageItem making it a modal

--- src/components/constructionPageItem.tsx
+++ src/components/constructionPageItem.tsx
@@ -1,4 +1,11 @@
 import Image from 'next/image'
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog'
 
 interface ConstructionPageItemProps {
   src: string
@@ -14,16 +21,39 @@ export function ConstructionPageItem({
   description,
 }: ConstructionPageItemProps) {
   return (
-    <div className="flex flex-col">
-      <Image
-        src={src}
-        alt={alt}
-        width={1760}
-        height={800}
-        className="w-full rounded-lg max-h-40"
-      />
-      <h2 className="mt-2 font-semibold text-lg">{title}</h2>
-      <p className="mt-1 text-md">{description}</p>
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="cursor-pointer">
+          <div className="flex flex-col">
+            <Image
+              src={src}
+              alt={alt}
+              width={1760}
+              height={800}
+              className="w-60 rounded-lg max-h-40"
+            />
+            <h2 className="mt-2 text-sm sm:text-lg">
+              <strong>{title}</strong>
+            </h2>
+            <p className="mt-1 text-xs sm:text-sm">{description}</p>
+          </div>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="max-w-3/4 sm:max-w-[825px] p-6">
+        <DialogTitle className="flex flex-col gap-4">
+          <Image
+            src={src}
+            alt={alt}
+            width={1760}
+            height={800}
+            className="w-full mt-4 rounded-lg"
+          />
+          {title}
+        </DialogTitle>
+        <DialogDescription>
+          <p>{description}</p>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
   )
 }
