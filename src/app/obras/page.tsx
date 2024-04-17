823c99b6f1aa0fef71c6c08666ef8551592e8a33
giovannivicentin <giovannifvicentin@gmail.com>
2024-04-17 14:48:40 -0300

    refactor: refactor the phrases and the title of constructionPage

--- src/app/obras/page.tsx
+++ src/app/obras/page.tsx
@@ -7,11 +7,11 @@ function ConstructionPage() {
     <div className="min-h-lvh mx-2 md:mx-32 pb-20 bg-background-color">
       <div className="animate-fade-right animate-once delay-200">
         <h1 className="pt-32 text-start text-2xl sm:text-3xl font-extrabold">
-          Construções Realizadas
+          Obras em destaque
         </h1>
         <p className="text-start leading-relaxed text-sm sm:text-lg mt-4">
-          Explore nossos projetos recentes e <strong>clique</strong> no que
-          chamar mais a sua atenção para ver mais detalhes.
+          Explore alguns dos nossos trabalhos entregues. Para observair mais
+          detalhes, clique sobre a imagem escolhida por você.
         </p>
       </div>
       <div className="mt-8 animate-fade-up grid grid-cols-2 sm:grid-cols-4 gap-4 duration-1000 delay-400">
