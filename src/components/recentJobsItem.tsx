5e4521c59138f379ec9cab8321b1ab3e96da0656
giovannivicentin <giovannifvicentin@gmail.com>
2024-05-13 16:41:15 -0300

    fix: correct a bug in the height of recent jobs

--- src/components/recentJobsItem.tsx
+++ src/components/recentJobsItem.tsx
@@ -15,13 +15,13 @@ export function RecentJobsItem({
   description,
 }: RecentJobsItemProps) {
   return (
-    <Card className="flex flex-col object-cover bg-background-color overflow-hidden border border-neutral-300">
+    <Card className="flex flex-col h-64 object-cover bg-background-color overflow-hidden border border-neutral-300">
       <Image
         src={src}
         alt={alt}
         width={1920}
         height={1080}
-        className="w-full rounded-se-lg rounded-sw-lg max-h-40 object-cover"
+        className="w-full rounded-se-lg rounded-sw-lg object-cover"
       />
       <CardTitle className="my-2 px-4">{title}</CardTitle>
       <CardDescription className="px-4 mb-2">{description}</CardDescription>
